8795b12e0ad32bf70c742dbcb0cc382cb28c1476
fernandovilla <fermvilla@gmail.com>
2026-08-12 16:17:23 -0300

    Continuação de implementação do cadastro de Company

---
+++ Udemy/ProjetoBlazor/Gestao.App/Gestao.App/wwwroot/js/app.js
@@ -0,0 +1,32 @@
+﻿function CompanyMaskInit() {
+    console.log("Carregando mascaras....");
+
+    LoadCNPJMask();
+
+    LoadCEPMask();    
+}
+
+const LoadCNPJMask = () => {
+    var cleaveCNPJ;
+
+    var cnpjMask = {
+        delimiters: [".", ".", "/", "-"],
+        blocks: [2, 3, 3, 4, 2],
+        uppercase: !0
+    }
+
+    document.querySelector("#cnpj") && (cleaveCNPJ = new Cleave("#cnpj", cnpjMask));
+}
+
+const LoadCEPMask = () => {
+
+    var cleaveCEP;
+
+    var cepMask = {
+        delimiters: [".", "-"],
+        blocks: [2, 3, 3],
+        uppercase: !0
+    }
+
+    document.querySelector("#cep") && (cleaveCEP = new Cleave("#cep", cepMask));
+}
