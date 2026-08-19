9de8805fb93b8fbc25962d18c62d443bae318c63
fernandovilla <fermvilla@gmail.com>
2026-08-18 22:37:33 -0300

    Implementado busca de CEP

---
+++ Udemy/ProjetoBlazor/Gestao.App/Gestao.App.Client/Libraries/Helpers/AuthenticationHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.NetworkInformation;
+using System.Security.Claims;
+
+namespace Gestao.App.Client.Libraries.Helpers
+{
+    public class AuthenticationHelper
+    {
+        public static async Task<Guid?> GetAuthenticatedUserIdAsync(AuthenticationStateProvider provider) 
+        {
+            var authenticationState = await provider.GetAuthenticationStateAsync();
+
+            var id = authenticationState.User.Claims.FirstOrDefault(a => a.Type == ClaimTypes.NameIdentifier);
+            
+            if (id != null && Guid.TryParse(id.Value, out var userId))
+                return userId;
+
+            return null;
+        }
+    }
+}
