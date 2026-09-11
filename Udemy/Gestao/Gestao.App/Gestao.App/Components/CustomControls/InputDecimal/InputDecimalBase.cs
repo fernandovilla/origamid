1d050e457a7f681a4f7fe0be20d425034748bac3
fernandovilla <fermvilla@gmail.com>
2026-09-11 17:19:46 -0300

    Documentação de decima input

--- Udemy/Gestao/Gestao.App/Gestao.App/Components/CustomControls/InputDecimal/InputDecimalBase.cs
+++ Udemy/Gestao/Gestao.App/Gestao.App/Components/CustomControls/InputDecimal/InputDecimalBase.cs
@@ -1,10 +1,10 @@
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 using System.Globalization;
 
-namespace Gestao.App.Components.CustomControls
+namespace Gestao.App.Components.CustomControls.InputDecimal
 {
-    public partial class InputDecimalBaseBCA : InputComponentBase
+    public class InputDecimalBase: InputComponentBase
     {
         private decimal? originalValue { get; set; }
         private decimal? value { get; set; }
